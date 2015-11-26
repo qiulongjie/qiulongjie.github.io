9e3bb2fb80cae24451a9a73df185a22aa22fc62a
qiulongjie <longjie_qiu@foxmail.com>
2015-11-26 10:58:15 +0800

    add baidu_tongji

---
+++ js/baidu/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?55ecc976b310dd77fef0ead042adf81c";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
